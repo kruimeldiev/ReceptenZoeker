f1dfdae48193d524f966778f20c88c52bae2cc43
Casper Daris <casperdaris@msn.com>
2021-01-05 22:26:32 +0100

    Nieuwe methode toegevoegd om de scrollview offet te berekenen

--- ReceptenZoeker/ViewControllers/ReceptDetailViewController.swift
+++ ReceptenZoeker/ViewControllers/ReceptDetailViewController.swift
@@ -12,45 +12,49 @@ class ReceptDetailViewController: UIViewController {
     // Omdat het recept van de VC wordt bepaald in de segue kan deze geforceerd worden
     var recept: ReceptViewModel!
     
-    @IBOutlet weak var receptFoto: UIImageView!
-    @IBOutlet weak var receptDetailLayer: UIView!
-    @IBOutlet weak var receptServingLayer: UIView!
-    @IBOutlet weak var receptNaamLabel: UILabel!
-    @IBOutlet weak var receptBronLabel: UILabel!
-    @IBOutlet weak var receptTijdLabel: UILabel!
-    @IBOutlet weak var receptServingsLabel: UILabel!
-    @IBOutlet weak var receptKcalLabel: UILabel!
+    var originalHeight: CGFloat!
+    
+    @IBOutlet weak var receptFotoView: UIImageView!
+    @IBOutlet weak var receptFotoViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var receptFotoVIewTop: NSLayoutConstraint!
+    @IBOutlet weak var detailViewScrollView: UIScrollView!
+    @IBOutlet weak var receptInfoView: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        makeDetailView(inputRecept: recept)
-    }
-    
-    func makeDetailView(inputRecept: ReceptViewModel) {
+        detailViewScrollView.delegate = self
+        
         recept.fetchImage() { foto in
-            self.receptFoto.image = foto
+            self.receptFotoView.image = foto
         }
-        self.receptDetailLayer.layer.cornerRadius = 20
-        self.receptServingLayer.layer.borderWidth = 0.3
-        self.receptServingLayer.layer.borderColor = UIColor.lightGray.cgColor
-        self.receptNaamLabel.text = recept.receptNaam
-        self.receptBronLabel.text = "Source: \(recept.bron)"
-        self.receptTijdLabel.text = "\(recept.bereidingTijd)'"
-        self.receptServingsLabel.text = String(recept.servings)
-        self.receptKcalLabel.text = "\(recept.kcal) kcal"
+        
+        self.receptInfoView.layer.cornerRadius = 20
+        
+        originalHeight = receptFotoViewHeight.constant
     }
     
-    @IBAction func minServingButton(_ sender: Any) {
-        if self.recept.servings > 1 {
-            self.recept.servings -= 1
-            viewDidLoad()
-        }
-    }
     
-    @IBAction func plusServingButton(_ sender: Any) {
-        self.recept.servings += 1
-        viewDidLoad()
+}
+
+extension ReceptDetailViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let offset = detailViewScrollView.contentOffset.y
+        let defaultTop = CGFloat(0)
+        var currentTop = defaultTop
+        
+        if offset < 0 {
+             currentTop = offset
+            receptFotoViewHeight.constant = originalHeight - offset
+        } else {
+            receptFotoViewHeight.constant = originalHeight
+        }
+        
+        receptFotoVIewTop.constant = currentTop
+        
     }
     
 }
